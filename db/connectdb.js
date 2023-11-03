84aa9be709ba909a8df0e167689afa003435bc04
sWD4 <shubham2405@gmail.com>
2023-11-03 22:34:36 +0530

    db

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://127.0.0.1:27017/blogproject'
+//const url = 'mongodb://127.0.0.1:27017/blogproject'
 const live_url ='mongodb+srv://sshubham2405:shubham1234@cluster0.yjekmrn.mongodb.net/apiecommerce?retryWrites=true&w=majority'
 const connectDB=()=>{
     return mongoose.connect(live_url)
